c27245ff00372ced05bf21a45200b129a556790d
jessicahayalkaratokus <hayal.karatokus@gmail.com>
2026-06-11 09:45:19 +0200

    Implement abstraction with Vehicle and Car classes

---
+++ abstraction.py
@@ -0,0 +1,19 @@
+# Abstraction Example
+
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
+
+    @abstractmethod
+    def start(self):
+        pass
+
+
+class Car(Vehicle):
+
+    def start(self):
+        print("Car engine started")
+
+
+car = Car()
+car.start()
